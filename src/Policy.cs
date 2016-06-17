ac94ba1fd00d41959e57f44c60fb3a36976040a0
xize <glucassen@live.nl>
2016-06-17 21:04:37 +0200

    refactored for better visibility

---
+++ src/Policy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace windows_tweak_tool
+{
+    interface Policy
+    {
+
+        string getName();
+
+        PolicyType getType();
+
+        string getDescription();
+
+        bool isEnabled();
+
+        void setEnabled();
+
+        void setDisabled();
+
+    }
+}
