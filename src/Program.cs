eac8fd6d1bf941889022f8a77d2d1f2efe904de3
xize <glucassen@live.nl>
2016-08-28 19:42:12 +0200

    add uac control to WTT

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -17,15 +20,46 @@ namespace windows_tweak_tool.src
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            gui = new window();
-            Application.Run(gui);
+            if(isElevated())
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                gui = new window();
+                Application.Run(gui);
+            } else
+            {
+                RunElevated(Application.ExecutablePath);
+            }
         }
 
         public static window getGui()
         {
             return gui;
+        }
+
+        public static bool isElevated()
+        {
+            WindowsPrincipal pricipal = new WindowsPrincipal(WindowsIdentity.GetCurrent());
+            if(pricipal.IsInRole(WindowsBuiltInRole.Administrator))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static void RunElevated(string fileName)
+        {
+            ProcessStartInfo processInfo = new ProcessStartInfo();
+            processInfo.Verb = "runas";
+            processInfo.FileName = fileName;
+            try
+            {
+                Process.Start(processInfo);
+            }
+            catch (Win32Exception)
+            {
+                //Do nothing. Probably the user canceled the UAC window
+            }
         }
     }
 }
