1d4215b48bf962889492a005257996b2d5df3f38
xize <glucassen@live.nl>
2016-09-15 23:10:26 +0200

    disable NTLM policy as it is not implemented yet

--- src/window.Designer.cs
+++ src/window.Designer.cs
@@ -333,6 +333,7 @@ namespace windows_tweak_tool
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.button1.AutoSize = true;
+            this.button1.Enabled = false;
             this.button1.Location = new System.Drawing.Point(420, 188);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
@@ -344,6 +345,7 @@ namespace windows_tweak_tool
             // 
             this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Enabled = false;
             this.progressBar1.Location = new System.Drawing.Point(11, 188);
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(403, 23);
