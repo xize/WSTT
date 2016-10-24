20b17ff9ec737c77699a3b4c9d9da1ea7f29ea7b
xize <glucassen@live.nl>
2016-10-24 19:21:19 +0200

    made the message more clear :)

--- src/window.cs
+++ src/window.cs
@@ -350,7 +350,7 @@ namespace windows_tweak_tool
         {
             Policy p = PolicyType.MBR_POLICY.getPolicy(this);
 
-            DialogResult result = MessageBox.Show("If you plan to use this feature please note that we have not developed MBRFilter\n\nMBRFilter is licensed under the GPLv2 License and maintained by Yves Younan, Cisco Talos at https://github.com/vrtadmin/MBRFilter\n\nby clicking \"yes\" you agree with the license", "Warning Third party driver", MessageBoxButtons.YesNo);
+            DialogResult result = MessageBox.Show("If you plan to use this feature please note that we have not developed MBRFilter\n\nMBRFilter is licensed under the GPLv2 License and maintained by Yves Younan, Cisco Talos at https://github.com/vrtadmin/MBRFilter\n\nby clicking \"yes\" you agree with their license", "Warning Third party driver", MessageBoxButtons.YesNo);
 
             if(result == DialogResult.No)
             {
