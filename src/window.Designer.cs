647915b63e9adfb287301001996fc04a11975685
xize <glucassen@live.nl>
2016-10-27 17:48:08 +0200

    add a message on the MBR policy button, why it is disabled

--- src/window.Designer.cs
+++ src/window.Designer.cs
@@ -927,11 +927,11 @@ namespace windows_tweak_tool
             this.mbrbtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.mbrbtn.AutoSize = true;
             this.mbrbtn.Enabled = false;
-            this.mbrbtn.Location = new System.Drawing.Point(419, 461);
+            this.mbrbtn.Location = new System.Drawing.Point(206, 461);
             this.mbrbtn.Name = "mbrbtn";
-            this.mbrbtn.Size = new System.Drawing.Size(75, 23);
+            this.mbrbtn.Size = new System.Drawing.Size(288, 23);
             this.mbrbtn.TabIndex = 87;
-            this.mbrbtn.Text = "Apply";
+            this.mbrbtn.Text = "disabled, has no uninstall way without damaging the MBR";
             this.mbrbtn.UseVisualStyleBackColor = true;
             this.mbrbtn.Click += new System.EventHandler(this.mbrbtn_Click);
             // 
@@ -941,7 +941,7 @@ namespace windows_tweak_tool
             | System.Windows.Forms.AnchorStyles.Right)));
             this.mbrprogress.Location = new System.Drawing.Point(10, 461);
             this.mbrprogress.Name = "mbrprogress";
-            this.mbrprogress.Size = new System.Drawing.Size(406, 23);
+            this.mbrprogress.Size = new System.Drawing.Size(190, 23);
             this.mbrprogress.TabIndex = 86;
             this.mbrprogress.Click += new System.EventHandler(this.mbrprogress_Click);
             // 
