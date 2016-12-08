0b6337a08e6a0a2d9f0cafb6e09f482580f77079
xize <glucassen@live.nl>
2016-12-08 16:16:35 +0100

    split policies with optional policies!

---
+++ src/optionalpolicies/ninite/NiniteCreator.cs
@@ -0,0 +1,89 @@
+﻿/*
+    A security toolkit for windows    
+
+    Copyright(C) 2016 Guido Lucassen
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.If not, see<http://www.gnu.org/licenses/>.
+*/
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace windows_tweak_tool.src.ninite
+{
+    class NiniteCreator : HashSet<NiniteOption>
+    {
+        
+        public string getNiniteURL()
+        {
+            string url = "https://ninite.com/";
+            NiniteOption[] options = this.ToArray();
+            
+            for (int index = 0; index < options.Length; index++)
+            {
+                if(index == (options.Length-1))
+                {
+                    NiniteOption option = options[index];
+                    url += ((NiniteOption)option).getName();
+                } else
+                {
+                    NiniteOption option = options[index];
+                    url += ((NiniteOption)option).getName()+"-";
+                }
+            }
+            return url+"/ninite.exe";
+        }
+
+        public void downloadNiniteInstaller(string url)
+        {
+            WebClient client = new WebClient();
+            client.Headers.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.59 Safari/537.36");
+            Directory.CreateDirectory(getDataFolder() + @"\ninite");
+            try
+            {
+                client.DownloadFile(new Uri(url), getDataFolder() + @"\ninite\ninite.exe");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                MessageBox.Show("Unable to download ninite!, perhaps the file has been removed?\nplease visit: " + url);
+                Process proc1 = Process.Start(url);
+                proc1.Dispose();
+                Directory.Delete(getDataFolder() + @"\ninite");
+                return;
+            }
+
+            Process proc = Process.Start(getDataFolder()+@"\ninite\ninite.exe");
+            
+            while(!proc.HasExited)
+            {
+                //lock thread
+            }
+            proc.Dispose();
+        }
+
+        private string getDataFolder()
+        {
+            return Config.getConfig().getDataFolder();
+        }
+
+    }
+}
---
+++ src/optionalpolicies/ninite/NiniteOption.cs
@@ -0,0 +1,94 @@
+﻿/*
+    A security toolkit for windows    
+
+    Copyright(C) 2016 Guido Lucassen
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.If not, see<http://www.gnu.org/licenses/>.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace windows_tweak_tool.src.ninite
+{
+    class NiniteOption
+    {
+
+        private static HashSet<NiniteOption> data = new HashSet<NiniteOption>();
+
+        public static NiniteOption SKYPE = new NiniteOption("skype", Program.getGui().getOptionalWindow().niniteskypecheckbox);
+        public static NiniteOption ESSENTIALS = new NiniteOption("essentials", Program.getGui().getOptionalWindow().niniteessentialscheckbox);
+        public static NiniteOption CLASSIC_START = new NiniteOption("classicstart", Program.getGui().getOptionalWindow().niniteclassiccheckbox);
+        public static NiniteOption WINSCP = new NiniteOption("winscp", Program.getGui().getOptionalWindow().winscpcheckbox);
+        public static NiniteOption THUNDERBIRD = new NiniteOption("thunderbird", Program.getGui().getOptionalWindow().ninitethunderbirdcheckbox);
+        public static NiniteOption MALWAREBYTES = new NiniteOption("malwarebytes", Program.getGui().getOptionalWindow().ninitembamcheckbox);
+        public static NiniteOption SEVENZIP = new NiniteOption("7zip", Program.getGui().getOptionalWindow().ninite7zipcheckbox);
+        public static NiniteOption PUTTY = new NiniteOption("putty", Program.getGui().getOptionalWindow().niniteputtycheckbox);
+        public static NiniteOption ITUNES = new NiniteOption("itunes", Program.getGui().getOptionalWindow().niniteitunescheckbox);
+        public static NiniteOption DROPBOX = new NiniteOption("dropbox", Program.getGui().getOptionalWindow().ninitedropboxcheckbox);
+        public static NiniteOption WINRAR = new NiniteOption("winrar", Program.getGui().getOptionalWindow().ninitewinrarcheckbox);
+        public static NiniteOption VLCPLAYER = new NiniteOption("vlc", Program.getGui().getOptionalWindow().ninitevlcplayercheckbox);
+        public static NiniteOption GOOGLE_DRIVE = new NiniteOption("googledrive", Program.getGui().getOptionalWindow().ninitegoogledrivecheckbox);
+        public static NiniteOption FILEZILLA = new NiniteOption("filezilla", Program.getGui().getOptionalWindow().ninitefilezillacheckbox);
+        public static NiniteOption SPOTIFY = new NiniteOption("spotify", Program.getGui().getOptionalWindow().ninitespotifycheckbox);
+        public static NiniteOption IMGBURN = new NiniteOption("imgburn", Program.getGui().getOptionalWindow().niniteimgburncheckbox);
+        public static NiniteOption NOTEPAD_PLUS_PLUS = new NiniteOption("notepadplusplus", Program.getGui().getOptionalWindow().ninitenotepadcheckbox);
+
+        private string name;
+        private CheckBox box;
+
+        private NiniteOption(string name, CheckBox box)
+        {
+            this.name = name;
+            this.box = box;
+            data.Add(this);
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public CheckBox getCheckbox()
+        {
+            return box;
+        }
+
+        public bool isEnabled()
+        {
+            return box.Checked;
+        }
+
+        public static NiniteOption valueOf(string name)
+        {
+            foreach(NiniteOption option in values())
+            {
+                if(name.ToLower().StartsWith(option.getName().ToLower()))
+                {
+                    return option;
+                }
+            }
+            throw new ArgumentNullException("cannot find "+name.ToLower()+" as option");
+        }
+
+        public static NiniteOption[] values()
+        {
+            return data.ToArray();
+        }
+
+    }
+}
