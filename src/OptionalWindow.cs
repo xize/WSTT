bf906ca2f18025d422d24bd7e86dba80cc24f753
xize <glucassen@live.nl>
2017-01-07 23:52:13 +0100

    move google chrome policies inside the optional options

--- src/OptionalWindow.cs
+++ src/OptionalWindow.cs
@@ -70,6 +70,9 @@ namespace windows_tweak_tool.src
         public CheckBox ninitethunderbirdcheckbox;
         private Button button1;
         private Button button2;
+        private Label label2;
+        public Button button3;
+        public ProgressBar progressBar1;
         public CheckBox niniteskypecheckbox;
 
         private void InitializeComponent()
@@ -108,6 +111,9 @@ namespace windows_tweak_tool.src
             this.niniteskypecheckbox = new System.Windows.Forms.CheckBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -479,7 +485,7 @@ namespace windows_tweak_tool.src
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.button1.Location = new System.Drawing.Point(13, 276);
+            this.button1.Location = new System.Drawing.Point(13, 326);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 90;
@@ -489,16 +495,52 @@ namespace windows_tweak_tool.src
             // button2
             // 
             this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.button2.Location = new System.Drawing.Point(94, 276);
+            this.button2.Location = new System.Drawing.Point(94, 326);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 91;
             this.button2.Text = "Unapply all";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 271);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 13);
+            this.label2.TabIndex = 98;
+            this.label2.Text = "add policies to google chrome:";
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.AutoSize = true;
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(420, 287);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 97;
+            this.button3.Text = "Apply";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Enabled = false;
+            this.progressBar1.Location = new System.Drawing.Point(11, 287);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(406, 23);
+            this.progressBar1.TabIndex = 96;
+            // 
             // OptionalWindow
             // 
-            this.ClientSize = new System.Drawing.Size(510, 311);
+            this.ClientSize = new System.Drawing.Size(510, 361);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.chromeaddonlabel);
