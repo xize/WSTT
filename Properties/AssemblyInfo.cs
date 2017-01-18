1b27ded9de37638c3ec8f0d00011a1caa9d69bef
xize <glucassen@live.nl>
2017-01-18 14:54:57 +0100

    trying to get the icon compiled

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(true)]
+[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("8a09e500-4dfa-4a20-b35d-7c7b0237b31a")]
