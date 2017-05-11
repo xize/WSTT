137eb3b0d2261752e07860b150981fe6cfc39c12
xize <glucassen@live.nl>
2017-05-11 15:59:17 +0200

    fix missing logo

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -61,16 +61,6 @@ namespace windows_tweak_tool.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap logo {
-            get {
-                object obj = ResourceManager.GetObject("logo", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized resource of type System.Byte[].
         /// </summary>
         internal static byte[] sigcheck {
