505c27aa22c09d45a323a3668bebfc534db9fdee
xize <glucassen@live.nl>
2017-05-18 15:21:23 +0200

    make sure the build year is correctly 2016-2017 from now.

--- src/OptionalWindow.cs
+++ src/OptionalWindow.cs
@@ -1,7 +1,7 @@
 ﻿/*
     A security toolkit for windows    
 
-    Copyright(C) 2017 Guido Lucassen
+    Copyright(C) 2016 Guido Lucassen
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
