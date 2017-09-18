18cca09aa3342154222d26ec758fccfe1dc3d344
xize <glucassen@live.nl>
2017-09-18 14:14:54 +0200

    make Chrome64bitPolicy async and fixed some bugs

--- src/optionalpolicies/Chrome64bitPolicy.cs
+++ src/optionalpolicies/Chrome64bitPolicy.cs
@@ -46,48 +46,79 @@ namespace windows_security_tweak_tool.src.optionalpolicies
             return CertProvider.GOOGLE_CHROME.getCertificate();
         }
 
-        public override void Apply()
+        public async override void Apply()
         {
             GetButton().Enabled = false;
-            if(!Directory.Exists(Config.GetConfig().GetDataFolder() + @"\wstt-downloaded"))
+            bool status = await Task.Run(()=>ApplyAsync());
+            if(status)
             {
-                Directory.CreateDirectory(Config.GetConfig().GetDataFolder() + @"\wstt-downloaded");
+                GetButton().Text = "undo";
+                GetProgressbar().Value = 100;
+            } else
+            {
+                GetButton().Text = "apply";
+                GetProgressbar().Value = 0;
             }
+            GetButton().Enabled = true;
+        }
+
+        public bool ApplyAsync()
+        {
+
+                if(!Directory.Exists(Config.GetConfig().GetDataFolder() + @"\wstt-downloaded"))
+                {
+                    Directory.CreateDirectory(Config.GetConfig().GetDataFolder() + @"\wstt-downloaded");
+                }
+                if (!Directory.Exists(Config.GetConfig().GetDataFolder() + @"\wstt-downloaded\chrome"))
+                {
+                    Directory.CreateDirectory(Config.GetConfig().GetDataFolder() + @"\wstt-downloaded\chrome");
+                }
 
             WebClient client = new WebClient();
-            client.DownloadFile("https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B03FE9563-80F9-119F-DA3D-72FBBB94BC26%7D%26lang%3Den%26browser%3D4%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable%26brand=GCEB/dl/chrome/install/GoogleChromeEnterpriseBundle64.zip", Config.GetConfig().GetDataFolder()+@"\wstt-downloaded\chrome.zip");
-            FileStream fs = File.OpenRead(Config.GetConfig().GetDataFolder() + @"\wstt-downloaded\chrome.zip");
+            client.DownloadFile("https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B03FE9563-80F9-119F-DA3D-72FBBB94BC26%7D%26lang%3Den%26browser%3D4%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable%26brand=GCEB/dl/chrome/install/GoogleChromeEnterpriseBundle64.zip", Config.GetConfig().GetDataFolder() + @"\wstt-downloaded\chrome\chrome.zip");
+            FileStream fs = File.OpenRead(Config.GetConfig().GetDataFolder() + @"\wstt-downloaded\chrome\chrome.zip");
             ZipArchive archive = new ZipArchive(fs);
-            archive.ExtractToDirectory(Config.GetConfig().GetDataFolder()+@"\wstt-downloaded\chrome");
+            archive.ExtractToDirectory(Config.GetConfig().GetDataFolder() + @"\wstt-downloaded\chrome");
+
             fs.Close();
             fs.Dispose();
             archive.Dispose();
 
             X509Certificate cert = X509Certificate.CreateFromSignedFile(Config.GetConfig().GetDataFolder() + @"\wstt-downloaded\chrome\Installers\GoogleChromeStandaloneEnterprise64.msi");
-            if(cert.GetCertHashString() == GetCertificate().getHash())
+            if (cert.GetCertHashString() == GetCertificate().getHash())
             {
                 Process p = Process.Start(Config.GetConfig().GetDataFolder() + @"\wstt-downloaded\chrome\Installers\GoogleChromeStandaloneEnterprise64.msi");
                 p.WaitForExit();
                 p.Dispose();
+
                 if (File.Exists(@"C:\Program Files (x86)\Google\Chrome\Application\Chrome.exe"))
                 {
-                    GetButton().Text = "undo";
-                    GetProgressbar().Value = 100;
+                    Directory.Delete(Config.GetConfig().GetDataFolder() + @"\wstt-downloaded\chrome", true);
+                    return true;
                 }
-                Directory.Delete(Config.GetConfig().GetDataFolder() + @"\wstt-downloaded\chrome", true);
-                File.Delete(Config.GetConfig().GetDataFolder() + @"\wstt-downloaded\chrome.zip");
-            } else
+        
+            }
+            else
             {
-               Directory.Delete(Config.GetConfig().GetDataFolder() + @"\wstt -downloaded\chrome", true);
-               File.Delete(Config.GetConfig().GetDataFolder() + @"\wstt-downloaded\chrome.zip");
+                Directory.Delete(Config.GetConfig().GetDataFolder() + @"\wstt-downloaded\chrome", true);
                 MessageBox.Show("The signed certificate did not match with the downloaded file!", "Invalid certificate or revoked!");
             }
+            return false;
+        }
+
+        public async override void Unapply()
+        {
+            GetButton().Enabled = false;
+
+            await Task.Run(() => UnapplyAsync());
+
+            GetButton().Text = "apply";
+            GetProgressbar().Value = 0;
             GetButton().Enabled = true;
         }
 
-        public override void Unapply()
+        public void UnapplyAsync()
         {
-            GetButton().Enabled = false;
             ProcessStartInfo info = new ProcessStartInfo("wmic.exe");
             info.CreateNoWindow = true;
             info.UseShellExecute = false;
@@ -95,9 +126,6 @@ namespace windows_security_tweak_tool.src.optionalpolicies
             Process p = Process.Start(info);
             p.WaitForExit();
             p.Dispose();
-            GetButton().Text = "apply";
-            GetProgressbar().Value = 0;
-            GetButton().Enabled = true;
         }
 
         public override Button GetButton()
