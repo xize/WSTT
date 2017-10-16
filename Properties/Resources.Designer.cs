119056f590694608f8014beebe4221ee90803005
xize <glucassen@live.nl>
2017-10-16 16:55:21 +0200

    fix missing logo in the HelpWindow

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -79,5 +79,15 @@ namespace windows_security_tweak_tool.Properties {
                 return ((byte[])(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap windowslogo {
+            get {
+                object obj = ResourceManager.GetObject("windowslogo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
